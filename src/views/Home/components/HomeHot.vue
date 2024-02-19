aaa40fcea064162bfc44e2ab21e04c581f4863ab
zjw <weizhao3682@gmail.com>
2024-02-19 10:54:34 +0800

    v0.6 homeImg

---
+++ src/views/Home/components/HomeHot.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+</script>
+
+<template>
+<div>home hot</div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/views/Home/components/HomeProduct.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+</script>
+
+<template>
+<div>home product</div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
