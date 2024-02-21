1d02455ccbe3702347618a2392f4d1964e388647
zjw <weizhao3682@gmail.com>
2024-02-21 16:28:29 +0800

    v1.1 基本功能完成

--- src/apis/user.js
+++ src/apis/user.js
@@ -1,13 +1,22 @@
 //封装用户相关的接口函数
 import httpInstance from "@/utils/http"
 
-export const loginAPI = ({account, password}) => {
+export const loginAPI = ({ account, password }) => {
     return httpInstance({
-        url:'/login',
+        url: '/login',
         method: 'POST',
         data: {
             account,
             password
+        }
+    })
+}
+
+export const getLikeListAPI = ({ limit = 4 }) => {
+    return httpInstance({
+        url: '/goods/relevant',
+        params: {
+            limit
         }
     })
 }
--- src/router/index.js
+++ src/router/index.js
@@ -12,6 +12,9 @@ import CartList from '@/views/CartList/index.vue'
 import Checkout from '@/views/Checkout/index.vue'
 import Pay from '@/views/Pay/index.vue'
 import PayBack from '@/views/Pay/PayBack.vue'
+import Member from '@/views/Member/index.vue'
+import UserInfo from '@/views/Member/components/UserInfo.vue'
+import UserOrder from '@/views/Member/components/UserOrder.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -24,24 +27,19 @@ const router = createRouter({
         {
           path: '',//default
           component: Home
-        },
-        {
+        }, {
           path: 'category/:id',
           component: Category
-        },
-        {
+        }, {
           path: 'category/sub/:id',
           component: SubCategory
-        },
-        {
+        }, {
           path: 'detail/:id',
           component: Detail
-        },
-        {
+        }, {
           path: 'cartlist',
           component: CartList
-        },
-        {
+        }, {
           path: 'checkout',
           component: Checkout
         }, {
@@ -50,6 +48,18 @@ const router = createRouter({
         }, {
           path: 'paycallback',
           component: PayBack
+        }, {
+          path: "member",
+          component: Member,
+          children: [
+            {
+              path: '',
+              component: UserInfo
+            }, {
+              path: 'order',
+              component: UserOrder
+            }
+          ]
         }
       ]
     },
